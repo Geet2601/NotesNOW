f75d6ac1559f73ca39676960a667355022a8da8f
Geet2601 <138841476+Geet2601@users.noreply.github.com>
2023-12-07 18:47:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPLAyXSnBchRXYiCS57aZuAegk7llNzdI",
-    authDomain: "notesnow-7b69d.firebaseapp.com",
-    projectId: "notesnow-7b69d",
-    storageBucket: "notesnow-7b69d.appspot.com",
-    messagingSenderId: "484281627480",
-    appId: "1:484281627480:web:aaa604bc4a773d237c2b2d",
-    measurementId: "G-QD11E5483D"
+   //your firebase api and stuff
   }
 
   if(!firebase.apps.length) {
